c7d300a801831c73b98dd75657dc5d71e1776dda
Allen Koo <crossgate9@gmail.com>
2019-04-23 15:32:43 +0800

    [add] custom category attribute category_h2

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,56 @@
+<?php
+namespace Raccoon\Utility\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        // $context->getVersion() is the old version, currect version
+        if (version_compare($context->getVersion(), '1.0.2') < 0) {
+            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Category::ENTITY,
+                'category_h2',
+                [
+                    'type' => 'text',
+                    'label' => 'Category H2',
+                    'input' => 'textarea',
+                    'sort_order' => 333,
+                    'source' => '',
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                    'visible' => true,
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => "",
+                    'group' => 'Content',
+                ]
+            );
+        }
+    }
+}
